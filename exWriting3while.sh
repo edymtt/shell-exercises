03edc13dda916d85c4f148c943669b98a61d9f34
Eric Miotto <edymtt@netscape.net>
2013-09-21 16:00:38 +0200

    Added 3rd exercise writing

---
+++ exWriting3while.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Listing the planets.
+
+planets=( Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune Pluto  )
+planet_index=0
+while [[ $planet_index< ${#planets[*]} ]]
+do
+  echo ${planets[planet_index]}  # Each planet on a separate line.
+(( planet_index++ ))
+done
+
+echo; echo
+
+planets_wrong[0]="Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune Pluto"  
+planet_index=0
+while [[ $planet_index < ${#planets_wrong[*]} ]]
+    # All planets on same line.
+    # Entire 'list' enclosed in quotes creates a single variable.
+    # Why? Whitespace incorporated into the variable.
+do
+  echo ${planets_wrong[planet_index]}
+(( planet_index++ ))
+done
+
+echo; echo "Whoops! Pluto is no longer a planet!"
+
+exit 0
